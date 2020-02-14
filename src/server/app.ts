e109c9037b567f3eeba748b5990dde96b1af6bb1
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-14 18:04:03 +1100

    rename file

---
+++ src/server/app.ts
@@ -0,0 +1,43 @@
+
+import * as express from 'express';
+import * as path from 'path';
+
+import * as apiRoutes from './routes/api';
+
+const app:express.Application = express();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+	(
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		console.info(`${req.method} ${req.path}`);
+		next();
+	}
+)
+
+// setup ejs
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/assets/', express.static(path.resolve(__dirname,'public','assets')));
+
+app.get('/', (_req:express.Request, res: express.Response) => {
+	res.render('index');
+});
+app.get('/exercise', (_req:express.Request, res: express.Response) => {
+	res.render('exercise');
+});
+
+app.use('/api', apiRoutes);
+
+app.use("*", (_req:express.Request, res: express.Response) => {
+	res.redirect('/');
+});
+
+export { app };
+
