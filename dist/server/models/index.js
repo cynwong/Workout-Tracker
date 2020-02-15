dc3b94bd9c06d2a6f90536f5160deb0534aeb069
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-15 20:13:31 +1100

    add generated files

---
+++ dist/server/models/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var workout_1 = require("./workout");
+exports.Workout = workout_1.Workout;
---
+++ dist/server/models/workout.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+var workoutSchema = new mongoose_1.Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Enter an exercise type',
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Enter an exercise name',
+            },
+            duration: {
+                type: Number,
+                required: 'Enter an exercise duration in minutes',
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ],
+}, {
+    toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true,
+    },
+});
+workoutSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce(function (total, exercise) { return total + exercise.duration; }, 0);
+});
+;
+exports.Workout = mongoose_1.model('Workout', workoutSchema);
